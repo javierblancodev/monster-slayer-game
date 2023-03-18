c3dc1ac2ba27505a54fc240de921df90773551fe
Javier <javierblancoit@gmail.com>
2023-03-18 12:16:06 +0000

    implement monster and player standard attackes

---
+++ app.js
@@ -0,0 +1,33 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
